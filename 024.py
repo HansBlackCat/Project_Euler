8931597acb85f5ebdedb81691855d50b3adb17e1
HansBlackCat <jeremy3599@seoul.ac.kr>
2019-11-21 15:11:15 +0900

    Lap_191121

--- 024.py
+++ 024.py
@@ -1,11 +1,39 @@
 #py3
 
-arr=[9,8,7,6,5,4,3,2,1,0]
-
-
-#3241 3214 3412 3421 4123
-""" Skeleton
-if arr[n]>arr[n+1]:
-    switch arr[n],[n+1]
+import scipy.special as sc
+import numpy as np
 
 """
+1,000,000
+3,628,800
+362,880
+40,320
+"""
+IPT=1000000
+
+i=[]
+
+def Cal(n,m):
+	ori=[i for i in range(m)]
+	arr=[]
+	m_ori=m
+	for cnr in range(m_ori):
+		for i in range(m):
+			if sc.factorial(m-1)*(i) < n <= sc.factorial(m-1)*(i+1):
+				np.sort(ori)
+				temp=ori[i]
+				break
+		arr.append(temp)
+		ori.remove(temp)
+		n=int(n-sc.factorial(m-1)*i)
+		m=m-1
+
+	print(n,m)
+	print(ori)
+	print(i)
+	print(arr)
+	return 0
+
+Cal(IPT,10)
+
+
