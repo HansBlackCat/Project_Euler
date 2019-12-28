979c526bd4fbd9a4b0955eeda3108c2f5cac8ad4
HansBlackCat <jeremy3599@snu.ac.kr>
2019-12-29 02:11:32 +0900

    ignore ignore

---
+++ haskell-euler/.stack-work/dist/e626a42b/build/haskell-euler-exe/autogen/Paths_haskell_euler.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE NoRebindableSyntax #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+module Paths_haskell_euler (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "C:\\Users\\Hans\\Git\\Project_Euler\\haskell-euler\\.stack-work\\install\\9f94fa9c\\bin"
+libdir     = "C:\\Users\\Hans\\Git\\Project_Euler\\haskell-euler\\.stack-work\\install\\9f94fa9c\\lib\\x86_64-windows-ghc-8.6.5\\haskell-euler-0.1.0.0-4jesustgDxYFrZiICxa9V8-haskell-euler-exe"
+dynlibdir  = "C:\\Users\\Hans\\Git\\Project_Euler\\haskell-euler\\.stack-work\\install\\9f94fa9c\\lib\\x86_64-windows-ghc-8.6.5"
+datadir    = "C:\\Users\\Hans\\Git\\Project_Euler\\haskell-euler\\.stack-work\\install\\9f94fa9c\\share\\x86_64-windows-ghc-8.6.5\\haskell-euler-0.1.0.0"
+libexecdir = "C:\\Users\\Hans\\Git\\Project_Euler\\haskell-euler\\.stack-work\\install\\9f94fa9c\\libexec\\x86_64-windows-ghc-8.6.5\\haskell-euler-0.1.0.0"
+sysconfdir = "C:\\Users\\Hans\\Git\\Project_Euler\\haskell-euler\\.stack-work\\install\\9f94fa9c\\etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "haskell_euler_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "haskell_euler_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "haskell_euler_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "haskell_euler_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "haskell_euler_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "haskell_euler_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "\\" ++ name)
